eb9918974b49520ad00c3c1086f11e599753f121
ezeportela <eportelab@gmail.com>
2017-06-08 13:40:16 -0300

    Agregue mensaje cuando no hay aplicacion

--- client/apps/applications.js
+++ client/apps/applications.js
@@ -2,6 +2,9 @@ Template.Applications.helpers({
   applications() {
     return Applications.find({ enabled: 1 }, { sort: { order: 1 }})
   },
+  applicationsAmount() {
+    return Applications.find({ enabled: 1 }, { sort: { order: 1 }}).count()
+  },
   isAdmin() {
     return Session.get('role') === 'admin'
   },
